5edd1cf27fa64c114ebf9da999190e34c8bd2853
Ricardo Sanchez <nardove@yahoo.com>
2024-04-30 21:54:12 +0100

    Add new style classes

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -3,19 +3,17 @@ import RenderUsersStandard from './components/RenderUsersStandard';
 import RenderUsersWithSWR from './components/RenderUsersWithSWR';
 import Controls from './components/Controls';
 
-import './App.css';
-
 function App() {
   return (
-    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 auto-rows-min md:auto-rows-fr h-full items-start px-4">
-      <div className="w-full md:w-10/12 mx-auto">
+    <div className="box-wrapper">
+      <div className="box-container">
         <RenderUsersStandard />
       </div>
-      <div className="w-full md:w-10/12 mx-auto">
+      <div className="box-container">
         <RenderUsersWithSWR />
       </div>
 
-      <div className="md:col-span-2 mx-auto w-full h-full content-center">
+      <div className="controls-container">
         <Controls />
       </div>
     </div>
